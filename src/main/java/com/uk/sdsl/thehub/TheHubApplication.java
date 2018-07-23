3934617aa5354ebb1fc651ac076ec6f9570babcb
Mohamed Mostafa <mmsa12@gmail.com>
2018-07-23 10:52:47 +0100

    Initial commit

---
+++ src/main/java/com/uk/sdsl/thehub/TheHubApplication.java
@@ -0,0 +1,12 @@
+package com.uk.sdsl.thehub;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TheHubApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TheHubApplication.class, args);
+    }
+}
